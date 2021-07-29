97f307c14b165018d1c863893c0bb0f50dd0989a
JaeWangL <jnso5072@outlook.kr>
2021-07-29 11:19:11 +0900

    [Feature] Add Fastify Extensions (Compress, Helmet, RateLimit, Multipart)

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,8 @@
+import FastifyCompress from 'fastify-compress';
+import FastifyHelmet from 'fastify-helmet';
+import FastifyMultipart from 'fastify-multipart';
+import FastifyRateLimiter from 'fastify-rate-limit';
+import { join } from 'path';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
@@ -15,6 +20,26 @@ import { AppModule } from '@modules/app/app.module';
 async function bootstrap() {
   const fAdapt = new FastifyAdapter({
     logger: false,
+  });
+  fAdapt.register(FastifyCompress);
+  fAdapt.register(FastifyRateLimiter, {
+    max: 100, // limit each IP to 100 requests per windowMs
+    timeWindow: 2 * 60 * 1000, // 2 minutes
+  });
+  fAdapt.register(FastifyHelmet, {
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: [`'self'`],
+        styleSrc: [`'self'`, `'unsafe-inline'`],
+        imgSrc: [`'self'`, 'data:', 'validator.swagger.io'],
+        scriptSrc: [`'self'`, `https: 'unsafe-inline'`],
+      },
+    },
+  });
+  fAdapt.register(FastifyMultipart);
+  fAdapt.useStaticAssets({
+    root: join(__dirname, '..', 'client'),
+    prefix: '/public/',
   });
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, fAdapt);
   app.useGlobalPipes(new ValidationPipe());
